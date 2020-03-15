10b6e20ea65dceb932e4d551455de150267de90c
JesseBoe <JesseBoe@gmail.com>
2020-03-15 14:41:41 -0700

    Sorting for datatables

--- client/src/components/ConsoleGameList/ConsolgeGameList.js
+++ client/src/components/ConsoleGameList/ConsolgeGameList.js
@@ -8,17 +8,18 @@ class ConsoleGameList extends React.Component {
         super(props);
         this.state = {
             GameList: [],
+            LastSort: ""
         }
     }
 
     componentDidMount() {
         this.callApi()
-            .then(res => (this.setState({GameList: res})))
+            .then(
+                res => (
+                    this.sortByNameFirst(res)
+                )
+            )
             .catch(err => console.log(err));
-    }
-
-    componentDidUpdate() {
-        $('#datatable').DataTable();
     }
 
     callApi = async () => {
@@ -31,7 +32,107 @@ class ConsoleGameList extends React.Component {
 
         return body;
     };
+
+    sortByNameFirst = (toSort) => {
+        toSort.sort(function (a, b) {
+            var textA = a.title.toUpperCase();
+            var textB = b.title.toUpperCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+        });
+        this.setState({ GameList: toSort, LastSort: "TitleAsc" });
+    }
+
+    sortByName = () => {
+        if (this.state.LastSort == "TitleAsc") {
+            let sorted = this.state.GameList;
+            sorted.sort(function (a, b) {
+                var textA = a.title.toUpperCase();
+                var textB = b.title.toUpperCase();
+                return (textA < textB) ? 1 : (textA > textB) ? -1 : 0;
+            });
+            this.setState({ GameList: sorted, LastSort: "TitleDsc" });
+        }
+        else {
+            let sorted = this.state.GameList;
+            sorted.sort(function (a, b) {
+                var textA = a.title.toUpperCase();
+                var textB = b.title.toUpperCase();
+                return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+            });
+            this.setState({ GameList: sorted, LastSort: "TitleAsc" });
+        }
+    }
+
+    sortByCreator = () => {
+        if  (this.state.LastSort == "CreatorAsc") {
+            let sorted = this.state.GameList;
+            sorted.sort(function (a, b) {
+                var textA = a.creator.toUpperCase();
+                var textB = b.creator.toUpperCase();
+                return (textA < textB) ? 1 : (textA > textB) ? -1 : 0;
+            });
+            this.setState({ GameList: sorted, LastSort: "CreatorDsc" });
+        }
+        else {
+            let sorted = this.state.GameList;
+            sorted.sort(function (a, b) {
+                var textA = a.creator.toUpperCase();
+                var textB = b.creator.toUpperCase();
+                return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+            });
+            this.setState({ GameList: sorted, LastSort: "CreatorAsc" });
+        }
+    }
+
+    sortByLastUpdate = () => {
+        if (this.state.LastSort == "UpdateAsc") {
+            let sorted = this.state.GameList;
+            sorted.sort(function (a, b) {
+                var textA = a.lastupdate.toUpperCase();
+                var textB = b.lastupdate.toUpperCase();
+                return (textA < textB) ? 1 : (textA > textB) ? -1 : 0;
+            });
+            this.setState({ GameList: sorted, LastSort: "UpdateDsc" });
+        }
+        else {
+            let sorted = this.state.GameList;
+            sorted.sort(function (a, b) {
+                var textA = a.lastupdate.toUpperCase();
+                var textB = b.lastupdate.toUpperCase();
+                return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+            });
+            this.setState({ GameList: sorted, LastSort: "UpdateAsc" });
+        }
+    }
     
+    getSortClasses = (colName) => {
+        if (colName == "Update") {
+            if (this.state.LastSort.includes(colName)) {
+                if (this.state.LastSort.includes("Asc")) {
+                    return "fas fa-sort-numeric-down sortIcon";
+                }
+                else {
+                    return "fas fa-sort-numeric-up sortIcon";
+                }
+            }
+            else {
+                return "fas fa-sort-numeric-down sortIcon sortIconFade";
+            }
+        }
+        else {
+            if (this.state.LastSort.includes(colName)) {
+                if (this.state.LastSort.includes("Asc")) {
+                    return "fas fa-sort-alpha-down sortIcon";
+                }
+                else {
+                    return "fas fa-sort-alpha-up sortIcon";
+                }
+            }
+            else {
+                return "fas fa-sort-alpha-down sortIcon sortIconFade";
+            }
+        }
+    }
 
     render() {
         return (
@@ -45,10 +146,10 @@ class ConsoleGameList extends React.Component {
                             <table id="datatable" className="table table-bordered table-dark">
                                 <thead>
                                     <tr>
-                                        <th scope="col"><b>Game</b></th>
+                                        <th onClick={() => { this.sortByName() }} scope="col" className="data-table-head"><b>Game</b><i className={this.getSortClasses("Title")}></i></th>
                                         <th scope="col"><b>Randomizer</b></th>
-                                        <th scope="col"><b>Creator</b></th>
-                                        <th scope="col"><b>Last Update</b></th>
+                                        <th onClick={() => { this.sortByCreator() }} scope="col" className="data-table-head"><b>Creator</b> <i className={this.getSortClasses("Creator")}></i></th>
+                                        <th onClick={() => { this.sortByLastUpdate() }} scope="col" className="data-table-head"><b>Last Update</b> <i className={this.getSortClasses("Update")}></i></th>
                                     </tr>
                                 </thead>
                                 <tbody>
